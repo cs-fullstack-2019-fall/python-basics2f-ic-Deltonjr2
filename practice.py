8f3f33a7c2b0656a90583fd9f793925056c0af4c
Deltonjr2 <Demetria.Farewell@yahoo.com>
2019-08-22 15:37:01 -0500

    classwork 082219

---
+++ practice.py
@@ -0,0 +1,80 @@
+# # love= input("Love you back")
+# import _random
+#
+# random_number= random.randint(1, 10)
+#
+# name_List= ["boib","joe","clark","james"]
+#
+# random_number = random.randint(0, len)(name_list)-1))
+#
+# print(name_List[random_number])
+#
+# rand_people= random.sample(name_List,1)
+#
+# print(rand_people)
+#
+# welcome_text=" Hello " + fname + "  " + 'from ' + city + '!!!'
+# welcome_text= ${fname} ${}
+
+
+# Python Basics: Math and String Formatting
+# # Problem 1:
+# #
+# # Write the code to create a random number from 1 - 100. Print the number generated.
+# #
+# # Hint:
+# # import random
+# # randomNumber = random.randint(?, ?)
+# # Problem 2:
+# #
+# # We will keep having this problem until EVERYONE gets it right without help
+# #
+# # Write some python code that has a loop that prompts the User for string input, and will only stop after the User enters q. If the user doesn't enter q, then prompt them to input another string. Continuing asking for input until they enter q.
+# #
+# # *NOTE: Your code should handle both lower and uppercase q values.
+# #
+# # Problem 3:
+# #
+# # Create a function that will loop until the user enters '0'. Ask the user to enter a positive integer number. Print 0 to that number and ask them again to enter a number until they enter '0'. Repeat.
+# #
+# # Problem 4:
+# #
+# # Write some Python code that creates a random number from 1 - 10 and stores it in a variable. Ask the user to guess the random number. Keep letting the user guess until they get it right, then quit. Print the message Sorry, incorrect for each wrong guess.
+# #
+# # Extra:
+# #
+# # But the range for the secret random number to 1 - 100. For each wrong guess, include a clue to the Guesser as to if their missing guess was 'too low' or 'too high'.
+#
+# 1.
+# import _random
+#
+# randomnumber = random.randint(0, 100)(-1)):
+#
+# print [randomnumber])
+#
+# randpeople= random.sample(1):
+#
+# print(randpeople)
+#
+# 1.
+n1= random.randint (1,100)
+print (rn1)
+#
+# userInput =""
+#
+2. while userInput !="q":
+    userInput=input ("Enter a string. q to quit")
+    userInput=userInput.upper()
+#
+# # 4.   Write some Python code that creates a random number from 1 - 10 and stores it in a variable. Ask the user to guess the random number. Keep letting the user guess until they get it right, then quit. Print the message Sorry, incorrect for each wrong guess.
+#
+# secret_num =random.randint(1,10)
+
+ # 4.
+while userguess != secretnum:
+    userguess= int(input("Enter your guess:")
+  if userguess != secretnum
+print('Incorrect.Try again')
+
+    else:
+     print("Correct")
